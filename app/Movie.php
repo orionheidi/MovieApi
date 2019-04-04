81dd6a0218dd99da6398c2cb0f36eb28f65822a4
orionheidi <mikimojamacka1983>
2019-04-04 19:40:35 +0200

    2

--- app/Movie.php
+++ app/Movie.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     const GENERES = ['horror','triler','crime','comedy','drama'];
+
+    protected $fillable = [
+        'title', 'director', 'imageUrl','duration','releaseDate','genere'
+    ];
 }
